a2de7cfaca6d27df38102346676beef655a481fb
Sebastien Chapuis <sebastien@chapu.is>
2021-09-28 13:12:56 +0200

    Use relative offsets on shaped directories

--- tezos/context/src/tezedge_context.rs
+++ tezos/context/src/tezedge_context.rs
@@ -100,6 +100,7 @@ impl TezedgeIndex {
         repo.get_value_from_offset(&mut storage.data, offset)?;
 
         Ok(Some(deserialize_object(
+            offset,
             // &storage.data,
             storage, &*repo,
         )?))
